cf50a64f378ab9a28e807259ed72620a9eaebd1a
Benis Tambe <will.jenifer@yahoo.com>
2021-08-16 12:57:56 -0400

    commit tree 567473e3ecc3a13352a7f8a2253ea8926fe186e7

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
 // Your code here
+
+function mapToNegativize(sourceArray){
+    const newArr =[];
+
+    for(let i = 0; i < sourceArray.length; i++){
+        newArr.push(sourceArray[i] - (sourceArray[i]*2));
+    }
+    return newArr;
+}
+
+function mapToNoChange(sourceArray){
+    const newArr =[];
+
+    for(let i = 0; i < sourceArray.length; i++){
+        newArr.push(sourceArray[i]);
+    }
+
+    return newArr;
+}
+function mapToDouble(sourceArray){
+    const newArr =[];
+
+    for(let i = 0; i < sourceArray.length; i++){
+        newArr.push(sourceArray[i] *2);
+    }
+    return newArr;
+}
+function mapToSquare(sourceArray){
+    const newArr =[];
+
+    for(let i = 0; i < sourceArray.length; i++){
+        newArr.push(sourceArray[i] **2);
+    }
+    return newArr;
+}
+
+
+function reduceToTotal(sourceArray, startingPoint =0){
+    let returnVal = startingPoint;
+
+    for(let i = 0; i < sourceArray.length; i++){
+        returnVal += sourceArray[i];
+    }
+    return returnVal;
+}
+
+
+function reduceToAllTrue(sourceArray){
+    let returnVal  = true;
+
+    for(let i = 0; i < sourceArray.length; i++){
+        if (!!!sourceArray[i] ){
+            returnVal  = false
+            return returnVal;
+        }
+    }
+    return returnVal;
+}
+
+function reduceToAnyTrue(sourceArray){
+    let returnVal  = false;
+
+    for(let i = 0; i < sourceArray.length; i++){
+        if (!!sourceArray[i] ){
+            returnVal  = true;
+            return returnVal;
+        }
+    }
+    return returnVal;
+}
